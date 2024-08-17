72b7350f4228d4f922acd31ea5a0e8430c6ba7e9
Janhavi Rewdekar <123866016+janhavi020@users.noreply.github.com>
2024-08-17 16:12:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 
